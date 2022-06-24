e2efb508f6da191634895fdb40f3eff406714d65
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-25 04:17:28 +0900

    作業ログを残すように設定

--- .github/workflows/chmod_files.py
+++ .github/workflows/chmod_files.py
@@ -5,6 +5,7 @@ def chmod_recursive(ftp: FTP_TLS, path: str):
   items = ftp.mlsd(path)
   for fname,opt in items:
     path_to_file = path + '/' + fname
+    print(path_to_file)
 
     if opt['type'] == 'dir':
       chmod_recursive(ftp, path_to_file)
@@ -12,8 +13,10 @@ def chmod_recursive(ftp: FTP_TLS, path: str):
       if fname.endswith('.sh'):
         if opt['unix.mode'] != '0700':
           ftp.sendcmd('SITE CHMOD 700 ' + path_to_file)
+          print('==> Permission has changed to 700')
       elif opt['unix.mode'] != '0604':
         ftp.sendcmd('SITE CHMOD 604 ' + path_to_file)
+        print('==> Permission has changed to 604')
 
 with FTP_TLS(
     host=argv[1],
