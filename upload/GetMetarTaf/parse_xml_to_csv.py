2d8de665edd43039439589493622149969b1a952
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-05 00:37:20 +0900

    他のpythonプログラムから呼ばれることを考慮して、機能を関数化した

--- upload/GetMetarTaf/parse_xml_to_csv.py
+++ upload/GetMetarTaf/parse_xml_to_csv.py
@@ -1,12 +1,29 @@
 import csv
+from dataclasses import dataclass
 from sys import stdin, stdout, argv
+from typing import TextIO
 from xml.etree import ElementTree
 
-METARs = ElementTree.parse(stdin).getroot().find("data").findall(argv[1])
+@dataclass
+class csv_columns:
+  station_id: str
+  raw_text: str
+  effected_date: str
 
-csv.writer(stdout).writerows([
-  [
-    metar.find("station_id").text,
-    metar.find("raw_text").text,
-    metar.find(argv[2]).text
-  ] for metar in METARs])
+  def toArr(self) -> list:
+    return [self.station_id, self.raw_text, self.effected_date]
+
+def parse(src: TextIO, target_elem_name: str, effected_date_elem_name: str) -> list[csv_columns]:
+  METARs = ElementTree.parse(src).getroot().find("data").findall(target_elem_name)
+
+  return [
+    csv_columns(
+      metar.find("station_id").text,
+      metar.find("raw_text").text,
+      metar.find(effected_date_elem_name).text
+    ) for metar in METARs]
+
+if __name__ == '__main__':
+  csv.writer(stdout).writerows(
+    [row.toArr() for row in parse(stdin, argv[1], argv[2])]
+  )
