6797f229763f864e2305f5a67eea3a4b8ba818ef
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-05 09:13:40 +0900

    Pythonで実行するように変更

--- upload/exec.sh
+++ upload/exec.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-GET_METAR_SH='GetMetarTaf/GetNoaaMetar.sh'
-GET_TAF_SH='GetMetarTaf/GetNoaaTaf.sh'
-
 export LANG=en_US.utf8
 
 cd `dirname $0`
 
-bash $GET_METAR_SH
-
-bash $GET_TAF_SH
+python3 ./GetMetarTaf/GetMetarTaf.py
